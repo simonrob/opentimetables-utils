c5898f41ba82e610dcf70380580261dcbc1aed3f
Simon Robinson <simon@robinson.ac>
2022-10-04 21:04:02 +0100

    Add new templates, paste option and online viewing mode Also improved event naming (titles rather than codes)

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Open Timetables Utilities
-Open Timetables allows institutions to easily provide public-facing timetables for all programmes. But it can be very difficult to extract data from its timetable display, and time-consuming to repeatedly adjust its interface to request schedule details. This repository contains utilities to help make this easier.
+[Open Timetables](http://opentimetables-help.azurewebsites.net/1.0/Content/Home.htm) allows institutions to easily provide public-facing timetables for all programmes.
+But it can be very difficult to extract data from its timetable display, and time-consuming to repeatedly adjust its interface to request schedule details.
+This repository contains utilities to help make this task easier.
 
 ## Disclaimer
-Double-check any outputs provided by these tools against the Open Timetables display to ensure accuracy. You are responsible for checking that results are correct.
+Double-check any outputs provided by these tools against the Open Timetables display to ensure accuracy.
+You are responsible for checking that results are correct.
 
 
 ## Installation
@@ -20,15 +23,34 @@ Generate an ICS (iCalendar) file from Open Timetables for a given list of module
 python extractics.py -m CSCM69 CSCM45 CSPM40
 ```
 
+Alternatively, use an online viewer to show the timetable for, say, next week:
+
+```shell
+python extractics.py -m CSCM69 -p next -o view
+```
+
+Or use `paste` mode to (attempt to) automatically extract codes from a table of module selections:
+
+```shell
+python extractics.py -m paste -o view
+```
+
 You can configure the script's behaviour via the following optional arguments:
-- `-m` or `--modules`: A list of module codes separated by spaces.
-- `-p` or `--period`: The time period to include in the output. Valid options are `year`, `s1` (i.e., Semester 1) or `s2` (Semester 2). Default: `year`.
-- `-o` or `--output-file`: The full path to an ICS file to save output. Default: `timetable.ics` in the same directory as the script.
+- `-m` or `--modules`: A list of module codes separated by spaces. Use the magic value `paste` and the script will prompt you to paste a list then do (very basic) automatic module code matching from, e.g., a module selection table.
+- `-p` or `--period`: The time period to include in the output. Valid options are `year`, `s1` (i.e., Semester 1), `s2` (Semester 2), `today`, `week` (current week) or `next` (next week). Default: `year` (i.e., all known events).
+- `-o` or `--output-file`: The full path to an ICS file to save output, or the magic value `view` to open the ICS output in an [online viewer](https://simonrob.github.io/online-ics-feed-viewer/). Default: `timetable.ics` in the same directory as the script.
 
 
 ## Limitations and caveats
-Module code searching is actually a string match on the module/course title (as codes are not provided anywhere else). So, including a module that shares a prefix with others will include all matches. Avoid this if needed by also appending the occurrence string (e.g., `CSCM69_A`).
+Module code searching is actually a string match on the module/course title (as codes are not provided anywhere else).
+So, including a module that shares a prefix with others will include all matches.
+Avoid this if needed by also appending the occurrence string (e.g., `CSCM69_A`).
 
+Calendar applications often import events from local ICS files rather than treating them as an updatable stream.
+If your timetabled events are likely to change, you may find it beneficial to set up the script to fetch events on a regular basis and store the resulting ICS file on a web server (or, e.g., Dropbox) that you can subscribe to.
+
+The default configuration in this repository is for Swansea University timetables and modules, but this can be configured by editing script global variables.
+Use a browser's developer tools to inspect and retrieve the correct configuration for your institution.
 
 ## License
 [Apache 2.0](LICENSE)
--- extractics.py
+++ extractics.py
@@ -3,13 +3,19 @@
 __author__ = 'Simon Robinson'
 __copyright__ = 'Copyright (c) 2022 Simon Robinson'
 __license__ = 'Apache 2.0'
-__version__ = '2022-10-03'  # ISO 8601 (YYYY-MM-DD)
+__version__ = '2022-10-04'  # ISO 8601 (YYYY-MM-DD)
 
 import argparse
 import asyncio
+import base64
 import datetime
+import io
 import json
 import os
+import re
+import sys
+import urllib.parse
+import webbrowser
 
 import aiohttp
 import icalendar
@@ -26,6 +32,9 @@ REQUEST_HEADERS = {
     'Authorization': AUTHORISATION_HEADER
 }
 
+E_START = '\u001b[31m'
+E_END = '\u001b[0m'
+
 
 class OpenTimetablesICS:
     def __init__(self, period):
@@ -33,12 +42,40 @@ class OpenTimetablesICS:
 
     @staticmethod
     def load_event_template(period='year'):
-        print('Loading event period template', period)
-        with open(f"{os.path.dirname(os.path.realpath(__file__))}/templates/{period}.json") as f:
-            return json.load(f)
+        """Timetable queries use a JSON template to specify the event period we are interested in"""
+        print(f"Loading event period template `{period}`")
+
+        period_name = 'week' if period == 'next' else period  # week templates share the same root
+        period_file = f"{os.path.dirname(os.path.realpath(__file__))}/templates/{period_name}.json"
+
+        if not os.path.exists(period_file):
+            print(f"\t{E_START}Error: time period template `{period}` not found; exiting{E_END}")
+            sys.exit(1)
+
+        with open(period_file) as f:
+            period_template = json.load(f)
+
+        if period_name == 'week' or period_name == 'today':
+            today = datetime.date.today()
+            start_date = today - datetime.timedelta(days=today.weekday()) + datetime.timedelta(
+                days=7 if period == 'next' else 0)
+            formatted_start_date = start_date.isoformat() + 'T00:00:00.000Z'
+            period_template['ViewOptions']['Weeks'][0]['FirstDayInWeek'] = formatted_start_date
+            if period_name == 'today':
+                current_day = today.weekday() + 1  # Monday is day 1 in Open Timetables; 0 in Python
+                period_template['ViewOptions']['Days'][0]['DayOfWeek'] = current_day
+                print(f"\tInitialised template to {formatted_start_date} (day {current_day})")
+            else:
+                print(f"\tInitialised template to {formatted_start_date} (one week)")
+        else:
+            # TODO: dynamic year and semester templates by calculating week dates? (currently needs annual updates)
+            pass
+
+        return period_template
 
     @staticmethod
     async def get_identifiers(session, module_codes):
+        """Each module has an identifier that can be retrieved through a search-type interface"""
         module_identifiers = {}
 
         for module_code in module_codes:
@@ -59,15 +96,17 @@ class OpenTimetablesICS:
                             print('\tAdding module result', name, ':', identity)
                             module_identifiers[name] = identity
                     if data['TotalPages'] > 1:
-                        print('\tWarning: found more than one page of results; additional pages will be skipped')
+                        print(f"\t{E_START}Warning: found more than one page of results; additional pages will be",
+                              f"skipped{E_END}")
 
             if not success:
-                print(f"\tWarning: module {module_code} not found at {BASE_URL}; skipping")
+                print(f"\t{E_START}Warning: module {module_code} not found at {BASE_URL}; skipping{E_END}")
 
         return module_identifiers
 
     @staticmethod
     async def get_timetable(session, event_template, identifier):
+        """Retrieve the actual timetable information using a period template and module identifier"""
         event_template['CategoryIdentities'] = [identifier]
         timetable = None
         async with session.post(f"{BASE_URL}broker/api/categoryTypes/{BASE_UUID}/categories/events/filter",
@@ -81,57 +120,91 @@ class OpenTimetablesICS:
         calendar.add('x-wr-calname', f"Lectures for modules {', '.join(module_codes)}")
         calendar.add('last-modified', datetime.datetime.now())
 
-        session = aiohttp.ClientSession()
-        module_identifiers = await self.get_identifiers(session, module_codes)
-        print('Downloading timetables for', len(module_identifiers), 'modules')
+        async with aiohttp.ClientSession() as session:
+            module_identifiers = await self.get_identifiers(session, module_codes)
+            print('Downloading timetables for', len(module_identifiers), 'modules')
 
-        for name, identifier in module_identifiers.items():
-            timetable = await self.get_timetable(session, self.event_template.copy(), identifier)
-            if not timetable:
-                print(f"\tWarning: timetable for module {name} not found at {BASE_URL}; skipping")
-                continue
+            for name, identifier in module_identifiers.items():
+                timetable = await self.get_timetable(session, self.event_template.copy(), identifier)
+                if not timetable:
+                    print(f"\t{E_START}Warning: timetable for module {name} not found at {BASE_URL}; skipping{E_END}")
+                    continue
 
-            event_count = 0
-            for opentimetables_event in timetable[0]['CategoryEvents']:
-                event = icalendar.Event()
-                event.add('summary', opentimetables_event['Name'])
-                event.add('dtstart', datetime.datetime.fromisoformat(opentimetables_event['StartDateTime']))
-                event.add('dtend', datetime.datetime.fromisoformat(opentimetables_event['EndDateTime']))
+                event_count = 0
+                for opentimetables_event in timetable[0]['CategoryEvents']:
+                    event = icalendar.Event()
+                    event.add('summary', name)
+                    event.add('dtstart', datetime.datetime.fromisoformat(opentimetables_event['StartDateTime']))
+                    event.add('dtend', datetime.datetime.fromisoformat(opentimetables_event['EndDateTime']))
 
-                description = f"Module name: {name}\nEvent type: {opentimetables_event['EventType']}"
-                if opentimetables_event['Location']:
-                    event.add('location', opentimetables_event['Location'])
-                    description += f"\nLocation: {opentimetables_event['Location']}"
-                for event_property in opentimetables_event['ExtraProperties']:
-                    if event_property['DisplayName'] == 'Photo':
-                        description += f"\nVenue photo: {event_property['Value']}"
-                event.add('description', description)
+                    description = f"Module code(s): {opentimetables_event['Name']}"
+                    description += f"\nModule name: {name}\nEvent type: {opentimetables_event['EventType']}"
+                    if opentimetables_event['Location']:
+                        event.add('location', opentimetables_event['Location'])
+                        description += f"\nLocation: {opentimetables_event['Location']}"
+                    for event_property in opentimetables_event['ExtraProperties']:
+                        if event_property['DisplayName'] == 'Photo':
+                            description += f"\nVenue photo: {event_property['Value']}"
+                    event.add('description', description)
 
-                calendar.add_component(event)
-                event_count += 1
+                    calendar.add_component(event)
+                    event_count += 1
 
-            print('\tAdded', event_count, 'scheduled activities for', name)
+                print('\tAdded', event_count, 'scheduled activities for', name)
 
-        print('Saving activities to', output_file)
-        with open(output_file, 'wb') as f:
-            f.write(calendar.to_ical())
-
-        await session.close()
+            if output_file == 'view':
+                print('Opening ICS viewer with timetable output')
+                ical_bytes = io.BytesIO(calendar.to_ical()).getvalue()
+                encoded_ics = urllib.parse.quote(base64.b64encode(ical_bytes))
+                webbrowser.open(f"https://simonrob.github.io/online-ics-feed-viewer/#file={encoded_ics}")
+            else:
+                print('Saving activities to', output_file)
+                with open(output_file, 'wb') as f:
+                    f.write(calendar.to_ical())
 
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('-m', '--modules', nargs='+', help='[Required] A list of module codes separated by spaces',
+    arg_parser.add_argument('-m', '--modules', nargs='+', help='[Required] A list of module codes separated by spaces, '
+                                                               'or the magic value `paste` to be prompted to paste a '
+                                                               'list from, e.g., a module selection table',
                             required=True)
     arg_parser.add_argument('-p', '--period', default='year', help='The time period to include in the output. Valid '
-                                                                   'options are `year`, `s1` (i.e., Semester 1) or '
-                                                                   '`s2` (Semester 2). Default: `year`')
+                                                                   'options are `year`, `s1` (i.e., Semester 1), `s2` '
+                                                                   '(Semester 2), `today`, `week` (current week) or '
+                                                                   '`next` (next week). Default: `year` (i.e., all'
+                                                                   'known events')
     arg_parser.add_argument('-o', '--output-file', default=OUTPUT_FILE, help=f"The full path to an ICS file to save "
-                                                                             f"output. Default: `{OUTPUT_FILE}` in the "
-                                                                             f"same directory as this script")
+                                                                             f"output, or the magic value `view` to "
+                                                                             f"open the ICS output in an online viewer "
+                                                                             f". Default: `{OUTPUT_FILE}` in the same "
+                                                                             f"directory as this script")
     arg_parser.add_argument('--version', action='version', version=__version__)
     args = arg_parser.parse_args()
 
+    if args.modules == ['paste']:
+        print('Please paste a list of module codes then press enter: ')
+        pasted_modules = []
+        while True:
+            line = input()
+            if line:
+                # try to match the various displays of module selections
+                match = re.match(r'^(?P<prefix>\w+\t)?(?P<code>[A-Z]+-?[A-Z]?\d+[A-Z]?)\t(?(prefix)\w|/)', line)
+                if match:
+                    pasted_modules.append(match.group('code'))
+            else:
+                break
+        if pasted_modules:
+            args.modules = pasted_modules
+        else:
+            print(f"{E_START}Error: unable to detect any module codes in pasted input; exiting{E_END}")
+            sys.exit(1)
+
+    print(f"Generating timetables for modules {args.modules}")
     timetable_parser = OpenTimetablesICS(args.period)
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(timetable_parser.generate_ical(args.modules, args.output_file))
+
+    try:
+        loop.run_until_complete(timetable_parser.generate_ical(args.modules, args.output_file))
+    except aiohttp.ClientConnectorError:
+        print(f"\t{E_START}Error retrieving timetable information - is there an internet connection?{E_END}")
